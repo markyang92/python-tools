ffd407eb0d8cd6edffe022d8a452772d25b121ea
DongHyeon Yang <pllpokko@alumni.kaist.ac.kr>
2021-11-05 21:28:29 +0900

    rm: algorithm, sw_test

--- algorithm/math/clock_number.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import sys
-
-if __name__ == '__main__':
-    readl=sys.stdin.readline()
-    N=int(readl().strip())
-    
--- sw_test/B3/B3.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-import sys
-
-if __name__ == '__main__':
-    N=list(sys.stdin.readline().strip())
