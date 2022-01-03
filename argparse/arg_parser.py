6760f49edfffee046d57bf2d417d05fc860effaa
mark.yang <mark.yang@lge.com>
2022-01-03 10:53:08 +0900

    Add subprocess, argparse

--- argparse/arg_parser.py
+++ argparse/arg_parser.py
@@ -3,17 +3,29 @@ import argparse
 import os
 import sys
 
+# ======================= BEGIN: Usage =================================== #
+PROG=os.path.normpath(sys.argv[0])
+usage=f'{PROG} [-h]  -d DISTRO -b BRANCH -n <BUILD_NO | latest> -i IMAGE -m MACHINE\n\n\
+simple usage:\n\
+  {PROG} --distro \n\
+Support DISTRO:\n\
+  hello world!'
+# ======================= END: Usage ===================================== #
+
 def parserFunc():
-    parser=argparse.ArgumentParser(prog='cloc_component.py',
-        usage='%(prog)s [-h] [--dry] -d DISTRO -b BRANCH -n BUILD_NO',
-        epilog='simple usage: %(prog)s --distro webos --branch master --build_no 1927')
-    parser.add_argument("--dry", help="Dry run script for specific target", action="store_true")
-    group2=parser.add_argument_group('mandatory option')
-    group2.add_argument("-d", "--distro", help="Pass Distro")
-    group2.add_argument("-b", "--branch", help="Pass Branch Name")
-    group2.add_argument("-n", "--build_no", help="Pass Build Number")
-    args=parser.parse_args()
-    print(args.dry)
+    global usage
+    parser = argparse.ArgumentParser(
+        usage = usage)
+    group2 = parser.add_argument_group('required option')
+    group2.add_argument("-d", "--distro", help="Pass Distro. You can specify 'git repository url'", required=True)
+    group2.add_argument("-b", "--branch", help="Pass Branch Name", required=True)
+    group2.add_argument("-n", "--build-no", help="Pass Build Number If you wan\'t to build latest build, Just pass 'latest'", required=True)
+    group2.add_argument("-i", "--image", help="Pass Build Image.", required=True)
+    group2.add_argument("-m", "--machine", help="Pass Build Machine", required=True)
+    args = parser.parse_args()
+    if len(sys.argv) <= 1:
+        parser.print_usage()
+        exit(1)
 
 if __name__ == '__main__':
     parserFunc()
--- subprocess/subprocess_skeleton.py
+++ subprocess/subprocess_skeleton.py
@@ -1,15 +1,44 @@
 #!/usr/bin/env python3
 import subprocess
+import sys
 
-def BASH_CMD(cmd: str):
-    proc=subprocess.Popen(cmd, shell=True, executable="/bin/bash", stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-    ret=proc.communicate()
-    if not proc.returncode:
-        return (0,ret[0])
+def BASH_CMD(cmd: str, STDIO_PIPE = False):
+    """
+    # ========================================================================= #
+    # @brief                Execute Simple bash command and deal with it
+    # @param cmd            Bash command : str
+    # @param STDIO_PIPE     IF Caller need 'STDOUT' string, Then it set to True
+    # @return (returncode, STDOUT | STDERR)
+    # ========================================================================= #
+    """
+    if STDIO_PIPE:
+        stdout = subprocess.PIPE
+        stderr = subprocess.PIPE
     else:
-        return (proc.returncode,ret[1])
+        stdout = sys.stdout
+        stderr = sys.stderr
+
+    proc = subprocess.Popen(cmd, shell=True, executable="/bin/bash", \
+        stdout = stdout, \
+        stderr = stderr, \
+        encoding = 'utf-8',
+        text = True)
+    ret = proc.communicate()
+    if not proc.returncode:
+        return (proc.returncode,ret[0]) # (returncode, STDOUT) : tuple
+    else:
+        return (proc.returncode,ret[1]) # (returncode, STDERR) : tuple
 
 
-if __name__ == '__main__':
-    ret=BASH_CMD('curl --max-time 3 -I wrongDNS.com')
-    print (ret)
+# Case 1. Don't Need STDOUT
+ret = BASH_CMD('echo \"hello world\"')
+if ret[0] != 0:
+    print(ret[1], file=sys.stderr)
+    exit(ret[0])
+
+# Case 2. Need STDOUT
+ret = BASH_CMD(f"find ./ -name \"file\"",STDIO_PIPE=True)
+if ret[0] != 0:
+    print(ret[1],file=sys.stderr)
+    exit(ret[0])
+res_stdout = ret[1].strip()
