8054e3b8d5efe07599cdd636d4f4c07d756e44fa
mark.yang <mark.yang@lge.com>
2022-11-02 10:53:29 +0900

    add file_IO for pattern matching

--- file_IO/file_io.py
+++ file_IO/file_io.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python3
 
-import sys
+import os
+import codecs
+
+
+def get_subpkgdata(pkg):
+    pkgdata = {}
+
+    def decode(str):
+        c = codecs.getdecoder("unicode_escape")
+        return c(str)[0]
+
+    if os.access(pkg, os.R_OK):
+        import re
+        with open(pkg, 'r') as f:
+            lines = f.readlines()
+        r = re.compile(r"(^.+?):\s+(.*)")
+        for l in lines:
+            m = r.match(l)
+            if m:
+                pkgdata[m.group(1)] = decode(m.group(2))
+    
+    return pkgdata
 
 if __name__=='__main__':
-    f=open('sample','r')
-
-    for line in f:
-        print(line)
-    
-    f.close()
+    pkg = 'which'
+    print(get_subpkgdata(pkg))
